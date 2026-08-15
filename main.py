40f579d9e39585f2e7d323d9d91aa98acf31c0f5
Andreas Terhoven <andreas.terhoven@live.de>
2026-08-15 14:01:40 +0200

    changed the cli output to be more detailed if a verbose flag is set

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if api_key == None:
 # use the argparse module to convert a command-line input into a prompt
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 # create a client
@@ -30,14 +31,11 @@ messages: list[ChatCompletionMessageParam] = [
 # get a response from the model with a hard-coded prompt from the command line
 response = client.chat.completions.create(model="openrouter/free", messages=messages, )
 
-# printing the hard-coded user prompt
-print(f"User prompt: {args.user_prompt}")
-
-# printing the number of tokens used after checking if it is not None
+# printing metadata if the verbose flag is set after checking if usage is not None
 if response.usage == None:
     raise RuntimeError("no Tokens used, check api key")
-
-print(f"Prompt tokens: {response.usage.prompt_tokens}\nResponse tokens: {response.usage.completion_tokens}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}\nPrompt tokens: {response.usage.prompt_tokens}\nResponse tokens: {response.usage.completion_tokens}")
 
 # printing the models response on the terminal
 print(response.choices[0].message.content)
